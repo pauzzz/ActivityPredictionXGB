09a349ec092443ebeaabf8fd61fe99da11f19a1c
Paul Wong <paulwong001@gmail.com>
2018-09-15 23:55:33 -0400

    added jupyter notebook for easy viewing

--- activity_predictor.py
+++ activity_predictor.py
@@ -1,50 +1,64 @@
-import numpy as np
+import pandas as pd
 import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from xgboost.sklearn import XGBClassifier
 
-xtrain=np.loadtxt('train/X_train.txt', delimiter=' ')
-ytrain=np.loadtxt('train/Y_train.txt')
+xtrain=pd.read_csv('train/X_train.csv', sep=' ', header=None)
+ytrain=pd.read_csv('train/Y_train.csv', header=None)
 
 xtrain.shape
 ytrain.shape
 
-xtest=np.loadtxt('test/X_test.txt', delimiter=' ')
-ytest=np.loadtxt('test/Y_test.txt')
+xtest=pd.read_csv('test/X_test.csv', sep=' ', header=None)
+ytest=pd.read_csv('test/Y_test.csv', header=None)
 
 xtest.shape
 ytest.shape
 
+#compare general non optimized params to optimized params
+
+tree=XGBClassifier(objective = 'multiclass:softmax' , learning_rate=0.1 ,n_estimators=1000)
+
+tree.fit(xtrain, ytrain)
 
 
-#xtrain=pd.DataFrame(xtrain)
-#xtrain['labels']=ytrain.tolist()
-#
-#xtest=pd.DataFrame(xtest)
-#xtest['labels']=ytest.tolist()
+pred_tree =  tree.predict(xtest)
 
-xgb_train=xgb.DMatrix(xtrain, label=ytrain)
-xgb_test=xgb.DMatrix(xtest, label=ytest)
+print(accuracy_score(ytest,pred_tree))
+
+#issues setting up classifier.
+#test cv params for best params, optimizing
+
+cv_params = {'n_estimators':[1000,1200],'max_depth':[3,5]}
+clf_params={'objective':'multiclass:softmax','nthread':2,'max_depth':3,'n_estimators':1000,'subsample':0.5}
+
+clf=xgb.XGBClassifier(clf_params)
+
+model= GridSearchCV(clf, cv_params, scoring='accuracy', cv=3, n_jobs=3)
 
 
-#YIKES tried to run this, it killed my computer. Try on a super computer :^)
-##test cv params for best params, optimizing
-#
-#cv_params1={'max_depth':[3,5,7], 'min_child_weight':[1,3,5]}
-#
-#ind_params1={'learning_rate': 0.1 , 'n_estimators':500, 'seed':123, 'subsample':0.8, 'colsample_bytree':0.8, 'objective':'multi:softmax' }
-#
-#optimized_GBM= GridSearchCV(xgb.XGBClassifier(**ind_params1), cv_params1, scoring='accuracy', cv=3, n_jobs=-1)
-#
-##now run our gridsearch with cross validation to see which params perform best.
-##should take a LONG time
-#optimized_GBM.fit(xtrain, ytrain)
-##output here:
-#
-#
-#
-##we use grid scores to determine best params.
-#optimized_GBM.grid_scores_
 
+#now run our gridsearch with cross validation to see which params perform best.
+#should take a LONG time
+model.fit(xtrain, ytrain)
+#output here:
+
+
+
+#we use grid scores to determine best params.
+model.grid_scores_
+
+#params for this first optimization are max_depth:3, n_estimators:1000.
+#now play with subsampling and learning rate params
+cv_params2={'learning_rate': [0.1,0.01], 'subsample': [0.5,1]}
+clf_params2={'n_estimators':1000, 'objective':'multiclass:softmax', 'max_depth':3, }
 #set up params for xgboost model
+clf2=XGBClassifier(clf_params2)
+model2=GridSearchCV(clf2, cv_params2, scoring='accuracy', cv=3, n_jobs=3, return_train_score=True)
+
+model2.fit(xtrain, ytrain)
+
 
 params={}
 params['objective'] = 'multi:softmax'
@@ -58,6 +72,9 @@ params['subsample']=0.8
 params['colsample_bytree']=0.7
 params['min_child_weight']=3
 
+
+xgb_train=xgb.DMatrix(xtrain, label=ytrain)
+xgb_test=xgb.DMatrix(xtest, label=ytest)
 
 watchlist = [(xgb_train, 'train'), (xgb_test, 'test')]
 num_round = 25
